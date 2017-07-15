8fa34186b552b78a3ba8b43c7335f3dda9cdaef6
Gordon McAllister <gordonmc8@gmail.com>
2017-07-15 23:01:00 +0100

    Install and setup Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: "http://localhost:4000/graphql"
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
